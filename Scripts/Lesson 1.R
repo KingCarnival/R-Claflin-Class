8ab1a9411fec2be96cf0dc6b43ef50dee107051b
KingCarnival <pb.itoldyouso@gmail.com>
2018-10-22 19:49:12 -0400

    Cheat sheets

--- Scripts/Lesson 1.R
+++ Scripts/Lesson 1.R
@@ -3,3 +3,6 @@ dotaTable <-read.csv("Datasets/Dota 2/match.csv")
 
 TimeByFirstBloodGraph<-ggplot(dotaTable, aes(dotaTable$duration,dotaTable$first_blood_time))+geom_point()
 print(TimeByFirstBloodGraph)
+
+TimeByFirstBloodGraph2<-ggplot(dotaTable, aes(dotaTable$duration,dotaTable$first_blood_time, color = dotaTable$radiant_win))+geom_point()
+print(TimeByFirstBloodGraph2)

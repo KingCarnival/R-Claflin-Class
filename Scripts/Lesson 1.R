cb09ad3915d4bcfee53da1c63d5bbf69f68ddd12
KingCarnival <pb.itoldyouso@gmail.com>
2018-10-22 20:04:30 -0400

    Third Stage

--- Scripts/Lesson 1.R
+++ Scripts/Lesson 1.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+
+# Data Set from https://www.kaggle.com/devinanzelmo/dota-2-matches#match.csv
 dotaTable <-read.csv("Datasets/Dota 2/match.csv")
 
 TimeByFirstBloodGraph<-ggplot(dotaTable, aes(dotaTable$duration,dotaTable$first_blood_time))+geom_point()
@@ -6,3 +8,6 @@ print(TimeByFirstBloodGraph)
 
 TimeByFirstBloodGraph2<-ggplot(dotaTable, aes(dotaTable$duration,dotaTable$first_blood_time, color = dotaTable$radiant_win))+geom_point()
 print(TimeByFirstBloodGraph2)
+
+TimeByFirstBloodGraph3<-TimeByFirstBloodGraph2+geom_smooth(method = "lm",se= FALSE)
+print(TimeByFirstBloodGraph3)
